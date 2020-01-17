c75308bc030432051e3e2fc706f3df0e9f513dd8
Gautham Minhas <minhasg@github.com>
2020-01-16 22:09:23 -0800

    Added glew initialization step and added some logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,43 @@
 #define GLFW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/vec4.hpp>
 
 #include "Triangle.h"
 
+#include <iostream>
+
 int main()
 {
+    std::cout << "Initializing glfw\n";
 	if(!glfwInit())
 	{
+        std::cout << "Failed to initialize GLFW!\n";
 		return -1;
 	}
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Hurh", NULL, NULL);
 	glfwMakeContextCurrent(window);
-
+    
+    if(glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW!\n";
+        return -2;
+    }
+    
     Triangle tri;
     
+    std::cout << "Entering main loop.\n";
 	while(!glfwWindowShouldClose(window))
-	{
-		glfwPollEvents();
+	{   	
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        std::cout << "Drawing triangle.\n";
+        tri.draw();
+        
+
+        std::cout << "Swapping buffers.\n";
 		glfwSwapBuffers(window);
+        
 	}
 
 	glfwDestroyWindow(window);
